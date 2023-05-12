0739bae9c6dfc50880c9177fd6c237e295c78035
Sourcery AI <>
2023-05-12 06:51:43 +0000

    'Refactored by Sourcery'

--- src/test/python/gcloud/gcloud.py
+++ src/test/python/gcloud/gcloud.py
@@ -15,40 +15,54 @@ class GCInstance:
 		system(cmd)
 
 	def createCustom(self, cores=1, ram=1024):
-		self.create("custom-{}-{}".format(cores,ram))
+		self.create(f"custom-{cores}-{ram}")
 
 	#n1-highcpu-8
 	def create(self, machine="f1-micro", args = "--preemptible"):
 		self.delete()
-		self.local('gcloud beta compute --project=ivory-infusion-209508 instances create {} --zone=europe-west1-b --machine-type={} --subnet=default --network-tier=PREMIUM --no-restart-on-failure --maintenance-policy=TERMINATE {} --service-account=470010940365-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name=miaou,device-name=miaou,mode=rw,boot=yes'.format(self.instance, machine, args))
+		self.local(
+			f'gcloud beta compute --project=ivory-infusion-209508 instances create {self.instance} --zone=europe-west1-b --machine-type={machine} --subnet=default --network-tier=PREMIUM --no-restart-on-failure --maintenance-policy=TERMINATE {args} --service-account=470010940365-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --disk=name=miaou,device-name=miaou,mode=rw,boot=yes'
+		)
 
 
 	def stopScript(self, script):
-		self.local('gcloud compute --project {}  instances add-metadata {} --metadata-from-file shutdown-script={} --zone "{}"'.format(self.project, self.instance, script, self.zone))
+		self.local(
+			f'gcloud compute --project {self.project}  instances add-metadata {self.instance} --metadata-from-file shutdown-script={script} --zone "{self.zone}"'
+		)
 
 	def start(self):
-		self.local('gcloud compute --project "{}" instances start --zone "{}" "{}"'.format(self.project, self.zone, self.instance))  # --machine-type=f1-micro
+		self.local(
+			f'gcloud compute --project "{self.project}" instances start --zone "{self.zone}" "{self.instance}"'
+		)
 		time.sleep(60) 
 
 
 	def stopHours(self, hours):
-		self.remote('sudo shutdown -P +{}'.format(int(hours*60)))
+		self.remote(f'sudo shutdown -P +{int(hours * 60)}')
 
 	def stop(self):
 		self.remote('sudo shutdown -P now')
 
 	def delete(self):
-		self.local('gcloud compute --project "{}" instances delete "{}"  --zone "{}" --keep-disks all --quiet'.format(self.project, self.instance, self.zone))
+		self.local(
+			f'gcloud compute --project "{self.project}" instances delete "{self.instance}"  --zone "{self.zone}" --keep-disks all --quiet'
+		)
 
 	def remote(self, cmd):
-		self.local('gcloud compute --project "{}" ssh --zone "{}" "{}" -- "{}"'.format(self.project, self.zone, self.instance, cmd))
+		self.local(
+			f'gcloud compute --project "{self.project}" ssh --zone "{self.zone}" "{self.instance}" -- "{cmd}"'
+		)
 
 	def localToRemote(self, source, target):
-		self.remote("rm -rf {}".format(target))
-		self.local('gcloud compute --project "{}" scp --zone "{}" {} {}:{}'.format(self.project, self.zone, source, self.instance, target))
+		self.remote(f"rm -rf {target}")
+		self.local(
+			f'gcloud compute --project "{self.project}" scp --zone "{self.zone}" {source} {self.instance}:{target}'
+		)
 
 	def remoteToLocal(self, source, target):
-		self.remote("rm -rf {}".format(target))
-		self.local('gcloud compute --project "{}" scp --zone "{}" {}:{} {}'.format(self.project, self.zone, self.instance, source,  target))
+		self.remote(f"rm -rf {target}")
+		self.local(
+			f'gcloud compute --project "{self.project}" scp --zone "{self.zone}" {self.instance}:{source} {target}'
+		)
 
 #setsid nohup (sbt test;sudo poweroff) &> sbtTest.txt
